a3b8f2b37e317c981defe4bf00d699fbbca0c1b5
Masayuki Okumura <basashi@sannet.ne.jp>
2020-02-01 14:22:15 +0900

    Add try~catch~finally

--- iPodSimConsole/Program.cs
+++ iPodSimConsole/Program.cs
@@ -12,58 +12,75 @@ namespace iPodSimConsole
     {
         static void Main(string[] args)
         {
-            iTunesApp ita = new iTunesApp();
-            SerialPort sp = new SerialPort("COM5", 9600);
-            sp.Open();
-            string title = "";
-            while (true)
+            iTunesApp ita = null;
+            SerialPort sp = null;
+
+            try
             {
-                if (title != ita.CurrentTrack.Name)
+                ita = new iTunesApp();
+                sp = new SerialPort("COM5", 9600);
+                sp.Open();
+                string title = "";
+                while (true)
                 {
-                    title = ita.CurrentTrack.Name;
-                    Console.WriteLine(ita.CurrentTrack.Name);
+                    if (title != ita.CurrentTrack.Name)
                     {
-                        byte[] ba_title = Encoding.UTF8.GetBytes(title);
-                        Array.Resize(ref ba_title, ba_title.Length + 1);
-                        ba_title[ba_title.Length - 1] = 0x00;
-                        byte[] ba = makeAAPMessage(0x04, new byte[] { 0x00, 0x21 }, ba_title);
+                        title = ita.CurrentTrack.Name;
+                        Console.WriteLine(ita.CurrentTrack.Name);
+                        {
+                            byte[] ba_title = Encoding.UTF8.GetBytes(title);
+                            Array.Resize(ref ba_title, ba_title.Length + 1);
+                            ba_title[ba_title.Length - 1] = 0x00;
+                            byte[] ba = makeAAPMessage(0x04, new byte[] { 0x00, 0x21 }, ba_title);
+                            sp.Write(ba, 0, ba.Length);
+                        }
+                    }
+                    Console.WriteLine(ita.PlayerPositionMS.ToString());
+                    {
+                        int elapsedtime = ita.PlayerPositionMS;
+                        byte[] ba_elapsedtime = new byte[5];
+                        ba_elapsedtime[0] = 0x04;
+                        ba_elapsedtime[1] = (byte)((elapsedtime >> 24) & 0x0FF);
+                        ba_elapsedtime[2] = (byte)((elapsedtime >> 16) & 0x0FF);
+                        ba_elapsedtime[3] = (byte)((elapsedtime >> 8) & 0x0FF);
+                        ba_elapsedtime[4] = (byte)((elapsedtime >> 0) & 0x0FF);
+                        byte[] ba = makeAAPMessage(0x04, new byte[] { 0x00, 0x27 }, ba_elapsedtime);
                         sp.Write(ba, 0, ba.Length);
                     }
+                    Console.WriteLine(ita.PlayerState.ToString());
+                    {
+                        int length = ita.CurrentTrack.Duration * 1000;
+                        byte[] ba_length = new byte[4];
+                        ba_length[0] = (byte)((length >> 24) & 0x0FF);
+                        ba_length[1] = (byte)((length >> 16) & 0x0FF);
+                        ba_length[2] = (byte)((length >> 8) & 0x0FF);
+                        ba_length[3] = (byte)((length >> 0) & 0x0FF);
+                        int time = ita.PlayerPositionMS;
+                        byte[] ba_time = new byte[4];
+                        ba_time[0] = (byte)((time >> 24) & 0x0FF);
+                        ba_time[1] = (byte)((time >> 16) & 0x0FF);
+                        ba_time[2] = (byte)((time >> 8) & 0x0FF);
+                        ba_time[3] = (byte)((time >> 0) & 0x0FF);
+                        byte[] ba_param = new byte[9];
+                        Array.Copy(ba_length, 0, ba_param, 0, 4);
+                        Array.Copy(ba_time, 0, ba_param, 4, 4);
+                        ba_param[ba_param.Length - 1] = (byte)((ita.PlayerState == ITPlayerState.ITPlayerStatePlaying) ? 0x01 : 0x02);
+                        byte[] ba = makeAAPMessage(0x04, new byte[] { 0x00, 0x1D }, ba_param);
+                        sp.Write(ba, 0, ba.Length);
+                    }
+                    System.Threading.Thread.Sleep(500);
                 }
-                Console.WriteLine(ita.PlayerPositionMS.ToString());
+            }
+            catch (Exception)
+            {
+            }
+            finally
+            {
+                if (sp != null)
                 {
-                    int elapsedtime = ita.PlayerPositionMS;
-                    byte[] ba_elapsedtime = new byte[5];
-                    ba_elapsedtime[0] = 0x04;
-                    ba_elapsedtime[1] = (byte)((elapsedtime >> 24) & 0x0FF);
-                    ba_elapsedtime[2] = (byte)((elapsedtime >> 16) & 0x0FF);
-                    ba_elapsedtime[3] = (byte)((elapsedtime >> 8) & 0x0FF);
-                    ba_elapsedtime[4] = (byte)((elapsedtime >> 0) & 0x0FF);
-                    byte[] ba = makeAAPMessage(0x04, new byte[] { 0x00, 0x27 }, ba_elapsedtime);
-                    sp.Write(ba, 0, ba.Length);
+                    sp.Close();
                 }
-                Console.WriteLine(ita.PlayerState.ToString());
-                {
-                    int length = ita.CurrentTrack.Duration * 1000;
-                    byte[] ba_length = new byte[4];
-                    ba_length[0] = (byte)((length >> 24) & 0x0FF);
-                    ba_length[1] = (byte)((length >> 16) & 0x0FF);
-                    ba_length[2] = (byte)((length >> 8) & 0x0FF);
-                    ba_length[3] = (byte)((length >> 0) & 0x0FF);
-                    int time = ita.PlayerPositionMS;
-                    byte[] ba_time = new byte[4];
-                    ba_time[0] = (byte)((time >> 24) & 0x0FF);
-                    ba_time[1] = (byte)((time >> 16) & 0x0FF);
-                    ba_time[2] = (byte)((time >> 8) & 0x0FF);
-                    ba_time[3] = (byte)((time >> 0) & 0x0FF);
-                    byte[] ba_param = new byte[9];
-                    Array.Copy(ba_length, 0, ba_param, 0, 4);
-                    Array.Copy(ba_time, 0, ba_param, 4, 4);
-                    ba_param[ba_param.Length - 1] = (byte)((ita.PlayerState == ITPlayerState.ITPlayerStatePlaying) ? 0x01 : 0x02);
-                    byte[] ba = makeAAPMessage(0x04, new byte[] { 0x00, 0x1D }, ba_param);
-                    sp.Write(ba, 0, ba.Length);
-                }
-                System.Threading.Thread.Sleep(500);
+                ita = null;
             }
             /*
             {
@@ -95,11 +112,6 @@ namespace iPodSimConsole
                 byte[] ba = { 0xFF, 0x55, 0x0C, 0x04, 0x00, 0x1D, 0x00, 0x03, 0x7E, 0xD8, 0x00, 0x00, 0x22, 0xEB, 0x02, 0x6B };
                 sp.Write(ba, 0, ba.Length);
             }
-            */
-            /*
-            sp.Close();
-            ita.OnPlayerPlayEvent -= Ita_OnPlayerPlayEvent;
-            ita = null;
             */
         }
 
